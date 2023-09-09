d6162f578ea1789d67c2afc78b9830ab7c0d0979
David Kressin <david@garciagonzales.com>
2023-09-09 03:48:40 -0300

    Add site and base in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidkressin.github.io/Web/',
+  site: 'https://davidkressin.github.io',
+  base: '/Web',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
