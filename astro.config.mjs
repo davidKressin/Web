88432fe94c277cb736cced6b41d29a3d5e16a572
David Kressin <david@garciagonzales.com>
2023-09-13 01:08:00 -0300

    Change 5 site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidkressin.github.io/Web/',
+  site: 'https://davidkressin.github.io/',
   base: '/dist',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
