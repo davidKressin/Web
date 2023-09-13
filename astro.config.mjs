4024c7dd75bf5f32f4110ac8d622e14b3ce060b0
David Kressin <david@garciagonzales.com>
2023-09-13 01:24:27 -0300

    Change site:https://davidkressin.github.io base:/Web

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidkressin.github.io/Web/',
-  // base: '/Web',
+  site: 'https://davidkressin.github.io',
+  base: '/Web',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
