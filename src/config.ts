2d527c636ae7ebae40cc3fa353d54b2d6d82cb67
David Kressin <david@garciagonzales.com>
2023-12-23 06:27:38 -0300

    Add icon en base head

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'David Kressin';
-export const SITE_DESCRIPTION = 'Bienvenido a mi sitio web. Encontraras proyectos, blogs y muchas cosas interesantes.';
+export const SITE_DESCRIPTION = 'Bienvenido a mi sitio web. En él encontrarás proyectos, blogs y muchas cosas interesantes.';
